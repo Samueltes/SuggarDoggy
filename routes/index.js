1198ebc1a2bb0b5df8abb0d32c6ad4dacd72c8bf
Sarah Perrin <sarah-perrin@outlook.fr>
2018-04-19 11:27:15 +0200

    Ajout de la bdd au index.js fait par Pierre

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,85 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 
+
+/***************************************/
+/******* Partie Basse de donnée ********/
+/***************************************/
+
+/* Connection à notre Base de donnée */
+var options = { server: { socketOptions: {connectTimeoutMS: 5000 } }};
+mongoose.connect('mongodb://pierrot38:montoto38@ds147469.mlab.com:47469/suggardoggy',
+    options,
+    function(err)
+    { console.log("ACCES BDD", err); }
+);
+
+/****** Création de notre Base de donnée **********/
+var shopsSchema = mongoose.Schema(
+{
+    raisonSociale: String,
+    specialite: String,
+    adresse: String,
+    ville: String,
+    cp: Number,
+    image: String,
+    descriptif: String
+});
+var shopsModel = mongoose.model('shops', shopsSchema);
+
+var produitsSchema = mongoose.Schema(
+{
+    nom: String,
+    prix: Number,
+    allergies: String,
+    nombre: Number,
+    shopsId: String,
+
+});
+var produitsModel = mongoose.model('produits', produitsSchema);
+
+var commandesSchema = mongoose.Schema(
+{
+
+    prixTotal: Number,
+    prixLivraison: Number,
+    etatPaiement: String,
+    clientsNom: String,
+    clientsPrenom: String,
+    clientsAdresse: String,
+    clientsVille: String,
+    clientsCp: Number
+
+});
+var commandesModel = mongoose.model('commandes', commandesSchema);
+
+var commProduitsSchema = mongoose.Schema(
+{
+    produitsNom: String,
+    produitsPrix: Number,
+    nombre: Number,
+    commandeId: String
+
+});
+var commProduitsModel = mongoose.model('commandesproduits', commProduitsSchema);
+
+
+/***************************************/
+/********* GESTION DES ROUTER **********/
+/***************************************/
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/shop', function(req, res, next) {
-  res.render('shop');
+router.get('/repertoire', function(req, res, next) {
+  
+  res.render('repertoire');
 });
 
-router.get('/repertoire', function(req, res, next) {
-  res.render('repertoire');
+router.get('/shop', function(req, res, next) {
+  res.render('shop');
 });
 
 router.get('/basket', function(req, res, next) {
